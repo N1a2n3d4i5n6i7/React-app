39a67c4fbb8f253f50c9e0105759a21f4681cef4
N1a2n3d4i5n6i7 <nandhinir20@gmail.com>
2024-01-12 06:37:42 -0500

    new changes

--- src/components/RestoMenu.js
+++ src/components/RestoMenu.js
@@ -1,32 +1,40 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import axios from "axios";
+import Restorentcatagory from "./restorentcatagory";
+
 
 const RestoMenu = () => {
-    const [resmenuInfo,setresmenuInfo] = useState(null);
-
+    const [restomenuInfo,setresmenuInfo] = useState(null);
     const {resId} = useParams();
 
-    console.log(resId);
+   // console.log(resId);
     useEffect(() => {
         fetchMenu();
     }, []);
 
     const fetchMenu = async() => {
-    const menurespo = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=14.6818877&lng=77.6005911&restaurantId="+resId);
-    const jsonData =  await menurespo.json();
-    setresmenuInfo(jsonData);
- };
- // Destructing the data
- //const { name, city, areaName, cuisines } = resmenuInfo?.data?.cards[0]?.card?.card?.info;
+    const RestomenuData =  await axios.get("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=14.6818877&lng=77.6005911&restaurantId="+resId);
+    //const jsonData =  await menurespo.json();
+    setresmenuInfo(RestomenuData.data);
+    const itemcardslist = RestomenuData.data.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards;
+    console.log(itemcardslist);
+ }
+ return (
+  <div className="restomenubar">
+      <h2 className="restoname">{restomenuInfo?.data?.cards[0]?.card?.card?.info.name}</h2>
+      <h2 className="restoname">{restomenuInfo?.data?.cards[0]?.card?.card?.info.locality}</h2>
+      <h3 className="restoname">{restomenuInfo?.data?.cards[0]?.card?.card?.info.aggregatedDiscountInfo.header}</h3>
+      <h3 className="restoname">{restomenuInfo?.data?.cards[0]?.card?.card?.info.sla.deliveryTime} mins</h3>
 
-  return (
-        <div className="menu-pg">
-            <h1> { resmenuInfo?.data?.cards[0]?.card?.card?.info.name}</h1>
-             <p> {resmenuInfo?.data?.cards[0]?.card?.card?.info.cuisines.join(",")}</p>
-             <p> {resmenuInfo?.data?.cards[0]?.card?.card?.info.city} - {resmenuInfo?.data?.cards[0]?.card?.card?.info.areaName}</p>
-            
-        </div>
-    );
-}
+     <Restorentcatagory/>
+        
+
+  </div>
+  
+)
+ 
+};
 
 export default RestoMenu;
+
---
+++ src/components/restorentcatagory.js
@@ -0,0 +1,11 @@
+// we need to receive the catagory data from restomenu catagory and display the catagory items in this component.
+
+const Restorentcatagory = () => {
+   
+    return (
+        <div className="catagory">
+       <h1> hi </h1>
+        </div>
+    );
+}
+export default  Restorentcatagory;
